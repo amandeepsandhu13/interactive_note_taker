694f86af7f535c40cc97be1d9071d3856790f6b4
amandeep <samandeepkaue34@gmail.com>
2024-06-04 14:06:35 -0400

    added code to add notes

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 const port = process.env.port || 3000;
+
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -10,8 +17,61 @@ app.get('/',(req,res) => {
 });
 
 app.get('/notes', (req,res) => 
-    res.sendFile(path.join(__dirname,'Develop/public/notes.html'))
+    res.sendFile(path.join(__dirname,'public/notes.html'))
 );
+
+//Add a note
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a review`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text, note_id } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      // Obtain existing notes
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new note
+          parsedNotes.push(newNote);
+  
+          // Write updated note back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated Notes!')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in adding notes');
+    }
+  });
+  
 
 app.listen(`${port}`, () => {
     console.log(`ok , Server is running on http://localhost:${port}`);
