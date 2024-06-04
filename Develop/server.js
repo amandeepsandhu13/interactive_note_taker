fc87bca3e8f68ae6906f67bb1a5481e3ec6333ab
amandeep <samandeepkaue34@gmail.com>
2024-06-04 11:58:29 -0400

    moved file to develop folder

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.port || 3000;
+
+
+app.use(express.static('Develop/public'));
+
+app.get('/',(req,res) => {
+    res.send('hello world');
+});
+
+app.get('/notes', (req,res) => 
+    res.sendFile(path.join(__dirname,'Develop/public/notes.html'))
+);
+
+app.listen(`${port}`, () => {
+    console.log(`ok , Server is running on http://localhost:${port}`);
+})
